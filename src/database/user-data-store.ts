94663ea9a996378b792efcdec25b52d05fd897a5
KusalDemo <kusalgunasekara2002@gmail.com>
2025-01-25 12:41:43 +0530

    Fixed throwing errors issue

--- src/database/user-data-store.ts
+++ src/database/user-data-store.ts
@@ -1,22 +1,22 @@
 import IUser from "../models/IUser";
 import {User} from "../models/User";
 
-export const registerUser = async (user : User) => {
+export const registerUser = async (user: User) => {
 
     try {
         const fetchedUser = await IUser.findOne({email: user.email});
 
         if (!fetchedUser) {
             // const encryptedPassword = await bcrypt.hash(password, 10);
-
             const newUser = new User(user.name, user.email, user.password, user.residencies, user.inquiries);
 
             await IUser.create(newUser);
             return newUser;
-        }else{
-            console.log(`User already exists`);
+        } else {
+            throw new Error(`User already exists`);
         }
     } catch (error) {
-        console.log(`Error occurred: ${error}`);
+        throw error instanceof Error ? error : new Error(`Error occurred: ${error}`);
     }
 }
+
--- src/routes/user-routes.ts
+++ src/routes/user-routes.ts
@@ -1,20 +1,20 @@
 import express from 'express';
 import {registerUser} from "../database/user-data-store";
-import {IUser} from "../models/IUser";
 import {User} from "../models/User";
 
-let customerRouter = express.Router();
+let userRouter = express.Router();
 
-customerRouter.post("/login", async (req, res) => {
+userRouter.post("/register", async (req, res) => {
     try{
         const user: User = req.body;
         let response = await registerUser(user);
         res.send(`User logged in successfully : ${response}`);
     }catch (error){
         console.error(`Error occurred: ${error}`);
-        res.status(500).send(error);
+        res.status(500).send(`${error}`);
     }
 })
 
-export default customerRouter;
+
+export default userRouter;
 
