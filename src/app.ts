c1c5468d208216ddab5a16a1c5123c97506d633b
KusalDemo <kusalgunasekara2002@gmail.com>
2025-01-25 08:38:11 +0530

    Connected to the Database

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,22 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 let app = express();
+dotenv.config();
 
-app.listen(3000,()=>{
-    console.log(`Server up and running on port 3000`);
+app.use(express.json());
+
+mongoose
+    .connect(process.env.MONGO_URI as string)
+    .then(()=>{
+        console.log(`Connected to DB`);
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
+
+const port = process.env.PORT || 5000;
+app.listen(port,()=>{
+    console.log(`Server up and running on port ${port}`);
 })
