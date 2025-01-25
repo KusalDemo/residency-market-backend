0dd4d4fbfd9e1f9097507cd777bc451b39f1a41a
KusalDemo <kusalgunasekara2002@gmail.com>
2025-01-25 07:35:07 +0530

    Designed the Database Models (User model)

---
+++ src/models/IUser.ts
@@ -0,0 +1,20 @@
+import mongoose, {Schema} from 'mongoose';
+
+export interface IUser extends Document{
+    name: string;
+    email: string;
+    password: string;
+    residencies: mongoose.Schema.Types.ObjectId[]
+    inquiries: mongoose.Schema.Types.ObjectId[]
+}
+
+let userSchema = new Schema<IUser>({
+    name: {type:String, required: true},
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    residencies: [{type: mongoose.Schema.Types.ObjectId, ref: 'Residency'}],
+    inquiries: [{type: mongoose.Schema.Types.ObjectId, ref: 'Inquiry'}]
+});
+
+
+export default mongoose.model<IUser>('User', userSchema);
