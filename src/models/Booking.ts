e011e80b12725e25b3d442116be67e115a8e7f8a
KusalDemo <kusalgunasekara2002@gmail.com>
2025-01-26 12:05:18 +0530

    Updated booking class and model

--- src/models/Booking.ts
+++ src/models/Booking.ts
@@ -3,11 +3,15 @@ export class Booking {
     user: string;
     startDate: Date;
     endDate: Date;
+    total: number;
+    status: string;
 
-    constructor(residency: string, user: string, startDate: Date, endDate: Date) {
+    constructor(residency: string, user: string, startDate: Date, endDate: Date, total: number, status: string) {
         this.residency = residency;
         this.user = user;
         this.startDate = startDate;
         this.endDate = endDate;
+        this.total = total;
+        this.status = status;
     }
 }
--- src/models/IBooking.ts
+++ src/models/IBooking.ts
@@ -4,14 +4,18 @@ export interface IBooking extends Document{
     residency: mongoose.Schema.Types.ObjectId,
     user: mongoose.Schema.Types.ObjectId,
     startDate: Date,
-    endDate: Date
+    endDate: Date,
+    total: number,
+    status: string
 }
 
 let BookingSchema = new Schema({
     residency: {type: mongoose.Schema.Types.ObjectId, ref: 'Residency', required: true},
     user: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
     startDate: {type: Date, required: true},
-    endDate: {type: Date, required: true}
+    endDate: {type: Date, required: true},
+    total: {type: Number, required: true},
+    status: {type: String, required: true}
 });
 
 export default mongoose.model<IBooking>('Booking', BookingSchema);
