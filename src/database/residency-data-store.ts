dfeea4c86c1ac53f3e54b0ff183b9fe23c329d31
KusalDemo <kusalgunasekara2002@gmail.com>
2025-01-26 09:34:22 +0530

    Implemented create residency and tested without auth

--- src/database/residency-data-store.ts
+++ src/database/residency-data-store.ts
@@ -1,3 +1,82 @@
-import {IResidency} from "../models/IResidency";
+
 import {Residency} from "../models/Residency";
+import IResidency from "../models/IResidency";
+
+export const createResidency = async (residency : Residency)=>{
+    try{
+        await IResidency.create(residency);
+        return residency;
+    }catch (error){
+        throw error instanceof Error ? error : new Error(`Error occurred: ${error}`);
+    }
+}
+
+export const getResidencies = async ()=>{
+    try{
+        const residencies = await IResidency.find();
+        return residencies;
+    }catch (error){
+        throw error instanceof Error ? error : new Error(`Error occurred: ${error}`);
+    }
+}
+
+export const getResidencyById = async (id:string) => {
+    try{
+        const residency = await IResidency.findById(id);
+        return residency;
+    }catch (error){
+        throw error instanceof Error ? error : new Error(`Error occurred: ${error}`);
+    }
+}
+
+export const getResidenciesByUserId = async (id:string) => {
+    try{
+        //const residencies = await IResidency.findById(id).populate("owner", "name email");
+        const residencies = await IResidency.findById({owner: id}).populate("owner", "name email");
+        return residencies;
+    }catch (error){
+        throw error instanceof Error ? error : new Error(`Error occurred: ${error}`);
+    }
+}
+
+export const updateResidency = async (id:string, residency: Residency) => {
+    try{
+        let updatedResidency = await IResidency.findByIdAndUpdate(id, residency, {new: true});
+        if (!updatedResidency) {
+            throw new Error(`Residency with id ${id} not found`);
+        }
+        return updatedResidency;
+    }catch (error){
+        throw error instanceof Error ? error : new Error(`Error occurred: ${error}`);
+    }
+}
+
+export const deleteResidency = async (id:string) => {
+    try{
+        const deletedResidency = await IResidency.findByIdAndDelete(id);
+        if (!deletedResidency) {
+            throw new Error(`Residency with id ${id} not found`);
+        }
+        return deletedResidency;
+    }catch (error){
+        throw error instanceof Error ? error : new Error(`Error occurred: ${error}`);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- src/routes/residency-routes.ts
+++ src/routes/residency-routes.ts
@@ -1,11 +1,19 @@
 import express from 'express';
+import {createResidency} from "../database/residency-data-store";
+import {Residency} from "../models/Residency";
 
 
 const residencyRouter = express.Router();
 
 residencyRouter.post('/create', async (req, res) => {
     try{
-        console.log("Create residency triggered..")
+        const residencyToCreate: Residency = req.body;
+        const residencyPromise = await createResidency(residencyToCreate);
+        if (residencyPromise) {
+            res.send(residencyPromise);
+        }else{
+            res.status(400).send("Residency not created");
+        }
     }catch (error){
         res.status(500).send(error);
     }
